16553afb4de32f6edcd1e55d3b67c863f5cc29ce
Dmytro Melnyk <dmytro.melnyk.v@gmail.com>
2021-10-12 11:31:19 +0300

    add middleware error handler

---
+++ GradeBook.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GradeBook.API.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (exception)
+                {
+                    case ApplicationException _:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = exception?.Message });
+                await response.WriteAsync(result);
+            }
+            
+        }
+    }
+}
